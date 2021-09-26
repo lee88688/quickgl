6f1493057a71d54757b6e928778a5fcfb6d00d04
Jimmie Lee <lee88688@163.com>
2021-09-26 21:51:28 +0800

    feat: implement generateJS function

--- packages/lvgl-style-loader/src/generate.ts
+++ packages/lvgl-style-loader/src/generate.ts
@@ -1,10 +1,56 @@
+import lodash from 'lodash';
 import { StyleItem } from "./align";
+import { PART_SELECTOR, STATE_SELECTOR, STYLE_PROP } from "./constants";
 
 
 export function generateCLang(styleItem: StyleItem): string {
   return '';
 }
 
-export function generateJS(styleItem: StyleItem): string {
-  return '';
+interface JSStyleItem {
+  className: string;
+  selector: number;
+  attributes: {name: number, value: number}[];
+}
+
+function lvPct(x: number): number {
+  const lvCoordSetSpec = x => x | (1 << 13);
+  return (x < 0 ? lvCoordSetSpec(1000 - (x)) : lvCoordSetSpec(x))
+}
+
+export function generateJS(styleItem: StyleItem): JSStyleItem {
+  const jsStyleItem: JSStyleItem = {
+    className: styleItem.className,
+    selector: 0,
+    attributes: [],
+  };
+  
+  // selector
+  for (const state of styleItem.stateSelector) {
+    jsStyleItem.selector |= STATE_SELECTOR[state];
+  }
+  for (const part of styleItem.partSelector) {
+    jsStyleItem.selector |= PART_SELECTOR[part];
+  }
+
+  // attributtes
+  for (const attr of styleItem.attributes) {
+    const name = `LV_STYLE_${attr.name.toUpperCase()}`;
+    if (!(name in STYLE_PROP)) {
+      throw new Error(`className(${styleItem.className})'s attribute(${attr.name}) is not in lvgl style props!`);
+    }
+    switch(attr.type) {
+      case 'pixel': {
+        const value = parseInt(attr.value);
+        jsStyleItem.attributes.push({name: STYLE_PROP[name], value});
+        continue;
+      }
+      case 'percent': {
+        const value = parseInt(attr.value);
+        jsStyleItem.attributes.push({name: STYLE_PROP[name], value: lvPct(value)});
+        continue;
+      }
+    }
+  }
+  return jsStyleItem;
 }
