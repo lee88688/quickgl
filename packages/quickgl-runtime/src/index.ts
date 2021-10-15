e685b961e6beda3c9407b2760bcfcab02c2168fd
lee <lee88688@163.com>
2021-10-15 17:34:39 +0800

    feat: export render function

--- packages/quickgl-runtime/src/index.ts
+++ packages/quickgl-runtime/src/index.ts
@@ -1,1 +1,10 @@
+import * as React from 'react';
+import ReactReconciler = require("react-reconciler");
 import hostConfig from "./hostConfig";
+import './polyfill';
+
+export const LvglReactReconciler = ReactReconciler(hostConfig as any);
+
+export function render(element: React.ReactElement | null, container: any): void {
+  LvglReactReconciler.updateContainer(element, container);
+}
